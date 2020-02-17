d85e75983c670a6ec0e01f13caaa361324ede9c7
Igor Grabowski <aigor2789@gmail.com>
2020-02-17 11:53:29 +0100

    Add method to write product in Search in HomePage class

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -1,14 +1,18 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage {
     private WebDriverWait wait;
 
+    By searchInput = By.cssSelector("input[id='search_query_top']");
+    By searchSubmit = By.cssSelector("button[name='submit_search']");
+
     public HomePage(WebDriver driver) {
         super(driver);
-        wait = new WebDriverWait(driver, 9);
+        wait = new WebDriverWait(driver, 15);
     }
 
     public HomePage goTo(String url) {
@@ -16,5 +20,24 @@ public class HomePage extends BasePage {
         return new HomePage(driver);
     }
 
+    public HomePage findProductInSearch(String productName){
+        driver.findElement(searchInput).sendKeys(productName);
+        driver.findElement(searchSubmit).click();
+        return this;
+    }
+
 
 }
+
+
+
+// Choose product from HomePage
+//    Actions action = new Actions(driver);
+//       WebElement element =  driver.findElement(By.cssSelector("div[id='block_top_menu']>ul>li)"));
+//       action.moveToElement(element).click().build().perform();
+////        WebElement productText = driver.findElement(By.xpath("//a[contains(text(), 'T-shirts')])"));
+//
+//
+////        List<WebElement> list = driver.findElements(By.cssSelector("ul[id='homefeatured']>li>div>div:nth-child(2)>div[class='button-container']>a>span"));
+////        wait.until(ExpectedConditions.visibilityOfAllElements(list));
+////        list.get(0).click();
--- src/test/java/Tests/HomeTest.java
+++ src/test/java/Tests/HomeTest.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test;
 public class HomeTest extends BaseTest {
 
 
+    private String productName = "T-shirt";
 
     @Test
-    public void firstTest() {
-        HomePage homePage = new HomePage(driver).goTo(configuration.getBaseUrl());
-        System.out.println("Go to page works fine");
+    public void firstTest(){
+        HomePage homePage = new HomePage(driver).goTo(configuration.getBaseUrl())
+                .findProductInSearch(productName);
+
+
+
+        //write name product in search OK
+        // click icon search OK
+        // go to results Page
+        // open product info
+        // add to cart
     }
 
 }
