e4aed186b5cd14fd226e3ed1a48d20fee8c1531b
Igor Grabowski <aigor2789@gmail.com>
2020-02-18 14:45:39 +0100

    Add new test findProductWithWrongName

--- src/main/java/PageObjects/OrderConfirmationPage.java
+++ src/main/java/PageObjects/OrderConfirmationPage.java
@@ -2,12 +2,17 @@ package PageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OrderConfirmationPage extends BasePage {
     private WebDriverWait wait;
 
     By orderCompleteSelector = By.cssSelector("p[class='cheque-indent']");
+    By submitButton = By.cssSelector("p>button[type='submit']");
+    By priceAmount = By.cssSelector("span[class='price']");
+
+
 
 
     public OrderConfirmationPage(WebDriver driver) {
@@ -15,11 +20,21 @@ public class OrderConfirmationPage extends BasePage {
         wait = new WebDriverWait(driver, 15);
     }
 
+    public OrderConfirmationCompletePage confirmatOrder() {
+        wait.until(ExpectedConditions.elementToBeClickable(submitButton)).click();
+        return new OrderConfirmationCompletePage(driver);
+    }
+
     public String getOrderText() {
         String orderCompleteText = driver.findElement(orderCompleteSelector).getText();
         return orderCompleteText;
     }
 
+    public Double getTotalAmount() {
+        String price = wait.until(ExpectedConditions.visibilityOfElementLocated(priceAmount)).getText();
+        String priceAfterConvert = price.substring(1,price.length()).trim();
+        return Double.parseDouble(priceAfterConvert);
+    }
 
 
 
--- src/main/java/PageObjects/SearchResults.java
+++ src/main/java/PageObjects/SearchResults.java
@@ -3,6 +3,7 @@ package PageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class SearchResults extends BasePage {
     private WebDriverWait wait;
 
     By listProduct = By.cssSelector("ul.product_list>li>div>div>div>a>img");
+    By alertWarning = By.cssSelector("p.alert-warning");
 
 
     public SearchResults(WebDriver driver) {
@@ -24,6 +26,11 @@ public class SearchResults extends BasePage {
         listProductResult.size();
         listProductResult.get(0).click();
         return new ProductPage(driver);
+    }
+
+    public String getTextFromAlert() {
+       String alertText = wait.until(ExpectedConditions.visibilityOfElementLocated(alertWarning)).getText();
+       return alertText;
     }
 
 
